4cbdf0d29a0f8f9f1087f6bb7fea8c6d75811301
webFerrol <35032717+webferrol@users.noreply.github.com>
2024-06-03 10:47:59 +0200

    Update TMP_LanguageFriendLySelector.cs

--- scripts/TMP_LanguageFriendLySelector.cs
+++ scripts/TMP_LanguageFriendLySelector.cs
@@ -5,36 +5,29 @@ using TMPro;
 using System.Collections;
 using System.Collections.Generic;
 
-public class TMP_LanguageFriendLySelector : MonoBehaviour
+public class TMP_LanguageFriendlySelector : MonoBehaviour
 {
     public TMP_Dropdown DropdownLangComponent; // Asigna el TMP_Dropdown desde el inspector
     public List<Locale> supportedLocales; // Recuerda cargar en el inspector los locales
     [SerializeField] private Locale defaultLocale; // Esta opción por si queremos cargar un idioma por defecto
+
     void Start()
     {
-       
         if (defaultLocale)
         {
             LocalizationSettings.SelectedLocale = defaultLocale;
         }
 
-       
         var availableLocales = LocalizationSettings.AvailableLocales.Locales;
 
         if (supportedLocales.Count != 0)
         {
-
             LoadFriendlyLanguages();
-            // Debug.Log(supportedLocales.Count);
         }
-            
         else
         {
-            // Debug.Log('h');
             LoadLanguages(availableLocales);
         }
-
-
 
         DropdownLangComponent.onValueChanged.AddListener(delegate { OnLanguageChanged(DropdownLangComponent); });
 
@@ -45,41 +38,60 @@ public class TMP_LanguageFriendLySelector : MonoBehaviour
 
     private void LoadFriendlyLanguages()
     {
-        // Buscamos los locales diponibles
-
+        // Buscamos los locales disponibles
         DropdownLangComponent.options.Clear();
 
         foreach (var locale in supportedLocales)
         {
-            Debug.Log(locale);
             LocalizedString localizedLanguageName = new LocalizedString { TableReference = "LanguageNames", TableEntryReference = locale.Identifier.Code };
             DropdownLangComponent.options.Add(new TMP_Dropdown.OptionData(localizedLanguageName.GetLocalizedString()));
-            // Debug.Log(localizedLanguageName.GetLocalizedString());
-            DropdownLangComponent.RefreshShownValue();
-
         }
+
+        DropdownLangComponent.RefreshShownValue();
     }
 
     private void LoadLanguages(List<Locale> availableLocales)
     {
         DropdownLangComponent.options.Clear();
+
         foreach (var locale in availableLocales)
         {
-            // DropdownLangComponent.options.Add(new TMP_Dropdown.OptionData(locale.Identifier.Code));
             DropdownLangComponent.options.Add(new TMP_Dropdown.OptionData(locale.LocaleName));
         }
+
+        DropdownLangComponent.RefreshShownValue();
     }
 
-        public void OnLanguageChanged(TMP_Dropdown change)
+    public void OnLanguageChanged(TMP_Dropdown change)
     {
-        var selectedLocale = LocalizationSettings.AvailableLocales.Locales[change.value];      
+        var selectedLocale = LocalizationSettings.AvailableLocales.Locales[change.value];
         StartCoroutine(SetLocale(selectedLocale));
     }
-
 
     IEnumerator SetLocale(Locale locale)
     {
         yield return LocalizationSettings.InitializationOperation; // Espera a que la localización se inicialice
         LocalizationSettings.SelectedLocale = locale; // Cambia el idioma
-    }    
+
+        // Actualiza los nombres de los idiomas en el Dropdown
+        UpdateDropdownOptions();
+    }
+
+    private void UpdateDropdownOptions()
+    {
+        var availableLocales = LocalizationSettings.AvailableLocales.Locales;
+
+        if (supportedLocales.Count != 0)
+        {
+            LoadFriendlyLanguages();
+        }
+        else
+        {
+            LoadLanguages(availableLocales);
+        }
+
+        // Actualiza el valor seleccionado del Dropdown
+        int currentLocaleIndex = availableLocales.IndexOf(LocalizationSettings.SelectedLocale);
+        DropdownLangComponent.value = currentLocaleIndex;
+    }
 }
